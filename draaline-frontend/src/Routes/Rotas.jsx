eee75e5966361b9d4e024ab68d3dec03f2b8f74c
Lucas-SousaS <lucassousa4019@gmail.com>
2025-04-10 09:18:19 -0300

    feat: add routes services and login

--- draaline-frontend/src/Routes/Rotas.jsx
+++ draaline-frontend/src/Routes/Rotas.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from '../Pages/Home/Home';
 import Appointments from '../Pages/Appointments/Appointments';
+import Services from '../Pages/Services/Services';
+import Login from '../Pages/Login/Login';
 
 function Rotas() {
   return (
@@ -9,6 +11,8 @@ function Rotas() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/appointments" element={<Appointments />} />
+        <Route path="/services" element={<Services />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </Router>
   );
