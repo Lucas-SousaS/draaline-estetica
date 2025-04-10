a7ad5be3e7e9720a2dc69b1a3b6783df621f0920
Lucas-SousaS <lucassousa4019@gmail.com>
2025-04-10 09:17:29 -0300

    style: add image and presentation

--- draaline-frontend/src/Pages/Home/Home.jsx
+++ draaline-frontend/src/Pages/Home/Home.jsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import LayoutPage from '../../Layout/LayoutPage';
-
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+import img from "../../assets/foto.jpg"
+import style from "./Home.module.css"
 function Home() {
   return (
-    <LayoutPage>
-      <div>
-        <h1>ola</h1>
+    <div className="w-screen min-h-screen overflow-y-auto overflow-x-hidden">
+      <Header />
+      <div className={` ${style.box} w-screen flex justify-around items-center`}>
+        <div>
+          <h1 className='text-7xl'>
+            Frase de Efeito
+          </h1>
+          <button className='bg-[#EACDC3] text-xl p-3 '>Veja mais</button>
+        </div>  
+        <div className='rounded-2xl h-[600px] overflow-hidden '> 
+          <img className='h-[600px]' src={img} alt="foto biomedica" />
+        </div>
       </div>
-    </LayoutPage>
+      <Footer />
+    </div>
+
   );
 }
 
