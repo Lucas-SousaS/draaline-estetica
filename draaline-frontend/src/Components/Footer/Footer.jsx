9e8794416251d680344fc158593711fd0af2fdd4
Lucas Sousa Silva <lucassousa4019@gmail.com>
2025-04-10 21:04:12 -0300

    feat: create footer and add react-icons

--- draaline-frontend/src/Components/Footer/Footer.jsx
+++ draaline-frontend/src/Components/Footer/Footer.jsx
@@ -1,9 +1,28 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from "react-icons/fa";
+
 function Footer() {
-    return ( 
-        <footer className="w-screen flex fixed bottom-[0%]">
-            <h1>Rodape</h1>
-        </footer>
-     );
+  return (
+    <footer className="w-screen flex  bottom-[0%] justify-around gap-20 p-10">
+      <div className="flex flex-col gap-4 text-xl">
+        <div className="flex gap-4">
+          <span>₢ 2025</span>
+          <p>Privacy Policy</p>
+        </div>
+
+        <div>
+          <p>Termos de Serviços</p>
+        </div>
+      </div>
+
+      <div>
+        <nav className="flex items-center justify-center gap-4 text-3xl">
+          <FaInstagram className=" hover:text-[#EACDC3] cursor-pointer transition-all" />
+          <FaFacebook className=" hover:text-[#EACDC3] cursor-pointer transition-all" />
+          <FaLinkedin className=" hover:text-[#EACDC3] cursor-pointer transition-all" />
+        </nav>
+      </div>
+    </footer>
+  );
 }
 
 export default Footer;
